bddb5965dc923ada16180a13201d4d1a6cc40fc7
Douglas Alves Domingues <douglas.domingues@tiscode.com.br>
2019-07-20 16:59:57 -0300

    [fix] missing esModule declaration

--- index.js
+++ index.js
@@ -1,4 +1,4 @@
-
+Object.assign(exports, "__esModule", { value: true });
 const CONSOLE_ATTRIBUTES = {
 	BOLD: '\x1b[1m', DIM: '\x1b[2m', UNDERLINE: '\x1b[4m', BLINK: '\x1b[5m',
 	INVERT: '\x1b[7m', HIDDEN: '\x1b[8m', RESET: '\x1b[0m'
